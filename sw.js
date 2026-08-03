32bce5dbb63d2c2f4792fbd482ea154a8f2392b3
Joao Carreiro <joao.diogo.carreiro@gmail.com>
2026-08-03 14:09:27 +0000

    Aba Placa: programar a placa dentro da app, com o negocio guardado

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Objetivo: a app abrir sempre, mesmo sem rede (a rua não tem sempre 4G).
    Os dados ficam no localStorage do telemóvel, não passam por aqui. */
 
-const CACHE = "placas-v8";
+const CACHE = "placas-v9";
 const ESSENCIAIS = [
   "./",
   "./index.html",
